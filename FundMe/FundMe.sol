d65e54607ed5be9ab6bd55c8cf499cf118f0c0f2
FFFarhan <farhangandhi@gmail.com>
2024-09-13 09:54:12 +0530

    #5

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 //get funds
 //withdraw funds
 //set minimum spend amount
@@ -14,4 +16,22 @@ contract FundMe{
         //set min limit 
         require(msg.value > 2100000000000000, "Not Enough Eth");
     }
+
+    function getPrice() internal view returns (uint256) {
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
+    }
+    function getConversionRate() public {}
+
+
+
+
+
+
 }
