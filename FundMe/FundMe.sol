a5dc93a002acdce5c2b9fbcf6272c64bbfae0958
FFFarhan <farhangandhi@gmail.com>
2024-09-13 17:40:38 +0530

    #8

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 //get funds
 //withdraw funds
@@ -9,26 +8,51 @@ import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
 
+    using PriceConverter for uint256;
+    address public owner;
     uint256 public minimumUsd = 5e18;
 
 address[] public funders;
 mapping(address funder=>uint256 amount ) public addressToAmountFunded;
 
+constructor(){
+    owner == msg.sender;
+}
+
 
     function fund() public payable  {
-        require(getConversionRate(msg.value) >= minimumUsd, "Not Enough Eth");
+        require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender]  = addressToAmountFunded[msg.sender] + msg.value;
     }
-    function withdraw() public payable {
+    function withdraw() public onlyOwner {
         //allow users to send eth
         //set min limit 
+        // require(msg.sender == owner, "Must be owner");   // added as a modifier
+        for(uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex++){
+            address funder = funders[fundersIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array by making a new one
+        funders = new address[](0);
+
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+//call is preffered over transfer and send in most places
+
+        //call
+        (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
         
     }
-
-    
-
-
+modifier onlyOwner(){
+    require(msg.sender == owner, "Must be owner");
+        _;
+    }
 
 
 
--- FundMe/PriceConverter.sol
+++ FundMe/PriceConverter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-library priceConverter{
+library PriceConverter{
     function getPrice() internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
